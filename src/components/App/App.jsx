bb44bf09a59a3c6247bc471f91823f278d18db4e
Andrey Larikov <larikov174@gmail.com>
2022-02-10 14:04:27 +0500

    dev: add component structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Main from '../Main/Main';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
