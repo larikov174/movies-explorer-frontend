0bd394258ec657e239577d25d37b3200f7d5b804
Andrey Larikov <larikov174@gmail.com>
2022-02-10 20:37:15 +0500

    feat: add navTab section

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavTab.css';
 
 function NavTab() {
-  return <div>NavTab</div>;
+  const titles = [
+    { id: '#aboutProject', name: 'О проекте' },
+    { id: '#techs', name: 'Технологии' },
+    { id: '#student', name: 'Студент' },
+  ];
+
+  const getTitle = () =>
+    titles.map((title) => (
+      <Link className="nav-tab__link" to={title.id}>
+        {title.name}
+      </Link>
+    ));
+
+  return (
+    <section className="nav-tab">
+      <nav className="nav-tab__menu">
+        {getTitle()}
+      </nav>
+    </section>
+  );
 }
 
-export default NavTab ;
+export default NavTab;
