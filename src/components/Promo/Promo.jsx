14ebbd53d25ae2b97393bfbb7de34f8f4d18a135
Andrey Larikov <larikov174@gmail.com>
2022-02-10 20:09:57 +0500

    feat: add promo section

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Promo.css';
 
 function Promo() {
-  return <div>Promo</div>;
+  return (
+    <section className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+    </section>
+  )
 }
 
 export default Promo ;
