ce14067d875370aeb7e8f66e5cd0f609490c0de5
Andrey Larikov <larikov174@gmail.com>
2022-02-12 21:24:51 +0500

    feat: add portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Portfolio.css';
 
 function Portfolio() {
-  return <div>Portfolio</div>;
+  const links = [
+    { url: 'https://practicum.yandex.ru/', name: 'Статичный сайт' },
+    { url: 'https://practicum.yandex.ru/', name: 'Адаптивный сайт' },
+    { url: 'https://practicum.yandex.ru/', name: 'Одностраничное приложение' },
+  ];
+
+  const getLink = () =>
+    links.map((link) => (
+      <li key={link.name}>
+        <Link key={link.name} className="portfolio__text portfolio__link" to={link.url}>
+          <p className="portfolio__text portfolio__text_link">{link.name}</p>
+          <span className="portfolio__arrow" />
+        </Link>
+      </li>
+    ));
+
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__text portfolio__text_title">Портфолио</h2>
+      <ul className="portfolio__list">{getLink()}</ul>
+    </section>
+  );
 }
 
-export default Portfolio ;
+export default Portfolio;
