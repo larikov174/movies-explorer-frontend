96beb19d616adf1162d1397b6757e3052c6ae538
Andrey Larikov <larikov174@gmail.com>
2022-02-15 15:27:39 +0500

    feat: add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import { NavLink, Link, useLocation } from 'react-router-dom';
 import './Header.css';
@@ -6,12 +7,48 @@ function Header() {
   const location = useLocation().pathname;
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
-  const burgerMenu = () => (
+  const burgerMenu = () => {
+    const activeStyle = {
+      borderBottom: "1px solid #000",
+      paddingBottom: "4px"
+    };
+
+    return (
     <div className={`burger-menu ${isMenuVisible ? 'visible' : ''}`}>
       <div className="burger-menu__overlay" />
-      <div className="burger-menu__container">menu1</div>
+      <div className="burger-menu__container">
+      <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/"
+        >
+          Главная
+        </NavLink>
+        <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/movies"
+        >
+          Фильмы
+        </NavLink>
+        <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/savedMovies"
+        >
+          Сохраненные фильмы
+        </NavLink>
+        <NavLink
+        className="burger__link burger__link_profile"
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to="/profile"
+      >
+        Аккаунт
+        <span className="header__profile_decor" />
+      </NavLink>
+      </div>
     </div>
-  );
+  )};
 
   const burgerButton = () => (
     <>
@@ -38,24 +75,41 @@ function Header() {
     </header>
   );
 
-  const moviesPageHeader = () => (
+  const moviesPageHeader = () => {
+    const activeStyle = {
+      fontWeight: 500
+    };
+
+    return (
     <header className="header header_theme_light">
       <Link className="header__link header__link_logo" to="/" />
       <div className="header__menu header__menu_theme_light">
-        <NavLink className="header__link header__link_nav" to="/movies">
+        <NavLink
+          className="header__link header__link_nav"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/movies"
+        >
           Фильмы
         </NavLink>
-        <NavLink className="header__link header__link_nav" to="/savedMovies">
+        <NavLink
+          className="header__link header__link_nav"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/savedMovies"
+        >
           Сохраненные фильмы
         </NavLink>
       </div>
-      <NavLink className="header__link header__link_nav header__link_profile" to="/profile">
+      <NavLink
+        className="header__link header__link_nav header__link_profile"
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to="/profile"
+      >
         Аккаунт
         <span className="header__profile_decor" />
       </NavLink>
       {burgerButton()}
     </header>
-  );
+  )};
 
   return location === '/' ? mainPageHeader() : moviesPageHeader();
 }
