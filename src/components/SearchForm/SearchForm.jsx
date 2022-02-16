59ebf3f3e12b6b3f15426b5adb6435c7778318b9
Andrey Larikov <larikov174@gmail.com>
2022-02-17 00:12:12 +0500

    feat: add custom checkbox

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,17 +4,18 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <section className="search-form">
-      <div className="search-form___container search-form__container_search">
+      <div className="search-form__container search-form__container_search">
         <input className="search-form__input" type="text" placeholder="Фильм" />
         <button className="search-form__button" type="button">
           Найти
         </button>
       </div>
       <div className="search-form__container search-form__container_check">
-        <input className="search-form__check" type="checkbox" id="serchCheck" />
-        <label htmlFor="serchCheck" className="search-form__text">
-          Короткометражки
+        <label htmlFor="checkbox" className="search-form__label">
+          <input className="search-form__check" type="checkbox" id="checkbox" />
+          <div className="search-form__slider search-form__round" />
         </label>
+        <p className="search-form__text">Короткометражки</p>
       </div>
     </section>
   );
