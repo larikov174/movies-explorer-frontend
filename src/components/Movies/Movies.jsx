160ad17115dd5c3993cb989337089932482af2c3
Andrey Larikov <larikov174@gmail.com>
2022-02-21 14:08:11 +0500

    feat: add movies card list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,11 +2,13 @@ import './Movies.css';
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 function Movies() {
   return (
     <section className="movies">
       <SearchForm />
+      <MoviesCardList />
     </section>
   );
 }
---
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -0,0 +1,15 @@
+import './MoviesCardList.css';
+import React from 'react';
+
+function MoviesCardList() {
+  return (
+    <div className="movies-card-list">
+      <div className="movies-card-list__content" />
+      <div className="movies-card-list__action">
+        <button className="movies-card-list__button" type="button">Еще</button>
+      </div>
+      </div>
+  )
+}
+
+export default MoviesCardList;
