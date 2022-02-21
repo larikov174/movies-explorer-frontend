3df169f49255ff19e0149a853734a7e0472014e4
Andrey Larikov <larikov174@gmail.com>
2022-02-21 15:17:44 +0500

    feat: add movies card

---
+++ src/components/MoviesCard/MoveisCard.jsx
@@ -0,0 +1,21 @@
+import './MoviesCard.css';
+import React from 'react';
+
+function MoviesCard() {
+  return (
+    <article className="movies-card">
+      <div className="movies-card__poster">
+        <img className="movies-card__image" alt="Обложка фильма" />
+        <button className="movies-card__button" type="submit">
+          Сохранить
+        </button>
+      </div>
+      <div className="movies-card__info">
+        <h2 className="movies-card__title">Название фильма</h2>
+        <p className="movies-card__duration">1ч17м</p>
+      </div>
+    </article>
+  );
+}
+
+export default MoviesCard;
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,15 +1,24 @@
 import './MoviesCardList.css';
 import React from 'react';
+import MoviesCard from '../MoviesCard/MoveisCard';
 
 function MoviesCardList() {
   return (
     <div className="movies-card-list">
-      <div className="movies-card-list__content" />
+      <div className="movies-card-list__content">
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        </div>
       <div className="movies-card-list__action">
-        <button className="movies-card-list__button" type="button">Еще</button>
+        <button className="movies-card-list__button" type="button">
+          Еще
+        </button>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
 export default MoviesCardList;
