4bd85331e079960f7d6db84caee9d64be44e3330
Andrey Larikov <larikov174@gmail.com>
2022-02-23 00:29:50 +0500

    fix: optimize icons, saved movies edit logic

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,13 +1,42 @@
 import './SavedMovies.css';
-import React from 'react';
+import React, { useState } from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import SearchForm from '../SearchForm/SearchForm'
+import SearchForm from '../SearchForm/SearchForm';
+import defaultImage from '../../images/default-picture.png';
 
 function SavedMovies() {
+  const length = 2;
+  const moviesDB = Array(length).fill({
+    image: defaultImage,
+    description: 'Название фильма',
+    duration: '1ч 17м',
+  });
+  const [initData, setInitData] = useState(null);
+
+  setTimeout(() => {
+    setInitData(moviesDB);
+  }, 500);
+
+  const renderData = () => {
+    if (initData) {
+      return (
+        <>
+          <SearchForm />
+          <MoviesCardList initData={initData} />
+        </>
+      );
+    }
+    return (
+      <>
+        <SearchForm />
+        <SearchForm />
+      </>
+    );
+  };
+
   return (
     <section className="saved-movies">
-      <SearchForm />
-      <MoviesCardList />
+      {renderData()}
     </section>
   );
 }
