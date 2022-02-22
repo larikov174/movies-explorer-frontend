07742093ce12638fa819c19c83002167fb3f5720
Andrey Larikov <larikov174@gmail.com>
2022-02-23 01:40:46 +0500

    fix: edit input attr

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,12 +5,12 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 function SearchForm() {
   return (
     <section className="search-form">
-      <div className="search-form__container">
-        <input className="search-form__input" type="text" placeholder="Фильм" />
-        <button className="search-form__button" type="button">
+      <form className="search-form__container">
+        <input className="search-form__input" type="text" name="search" placeholder="Фильм" required />
+        <button className="search-form__button" type="submit">
           Найти
         </button>
-      </div>
+      </form>
       <FilterCheckbox />
     </section>
   );
