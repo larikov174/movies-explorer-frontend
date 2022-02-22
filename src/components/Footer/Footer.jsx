8cbfd5b15a514d892b8f38cafd0715f61e826039
Andrey Larikov <larikov174@gmail.com>
2022-02-22 13:48:52 +0500

    fix: edit footer showUp logic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   const currentDate = new Date();
-
+  const location = useLocation().pathname;
+  const allowedPages = ['/', '/movies', '/saved-movies'];
   const links = [
     { url: 'https://practicum.yandex.ru/', name: 'Яндекс.Практикум' },
     { url: 'https://github.com/larikov174', name: 'Github' },
@@ -18,15 +19,22 @@ function Footer() {
       </Link>
     ));
 
-  return (
-    <footer className="footer">
-      <p className="footer__text footer__text_credits">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <div className="footer__menu">
-        <p className="footer__text">&copy; {currentDate.getFullYear()}</p>
-        <nav className="footer__nav">{getLink()}</nav>
-      </div>
-    </footer>
-  );
+  const renderFooter = () => {
+    if (allowedPages.includes(location)) {
+      return (
+        <footer className="footer">
+          <p className="footer__text footer__text_credits">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+          <div className="footer__menu">
+            <p className="footer__text">&copy; {currentDate.getFullYear()}</p>
+            <nav className="footer__nav">{getLink()}</nav>
+          </div>
+        </footer>
+      );
+    }
+    return null;
+  };
+
+  return renderFooter();
 }
 
 export default Footer;
