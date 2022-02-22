0ba2cb6a144035ccbda14417dfeb3b604d8ed85f
Andrey Larikov <larikov174@gmail.com>
2022-02-23 03:00:38 +0500

    fix: burger menu code cleanUp

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -6,54 +6,51 @@ function BurgerMenu() {
   const location = useLocation().pathname;
   const allowedPages = ['/movies', '/saved-movies', '/profile'];
   const [isMenuVisible, setIsMenuVisible] = useState(false);
-
-  const burgerMenu = () => {
-    const activeStyle = {
-      borderBottom: '1px solid #000',
-      paddingBottom: '4px',
-    };
-
-    return (
-      <div className={`burger-menu ${isMenuVisible ? 'visible' : ''}`}>
-        <div className="burger-menu__overlay" />
-        <div className="burger-menu__container">
-          <NavLink
-            className="burger__link"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            to="/"
-            onClick={() => setIsMenuVisible(false)}
-          >
-            Главная
-          </NavLink>
-          <NavLink
-            className="burger__link"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            to="/movies"
-            onClick={() => setIsMenuVisible(false)}
-          >
-            Фильмы
-          </NavLink>
-          <NavLink
-            className="burger__link"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            to="/saved-movies"
-            onClick={() => setIsMenuVisible(false)}
-          >
-            Сохраненные фильмы
-          </NavLink>
-          <NavLink
-            className="burger__link burger__link_profile"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            to="/profile"
-            onClick={() => setIsMenuVisible(false)}
-          >
-            Аккаунт
-            <span className="header__profile_decor" />
-          </NavLink>
-        </div>
-      </div>
-    );
+  const activeStyle = {
+    borderBottom: '1px solid #000',
+    paddingBottom: '4px',
   };
+
+  const burgerMenu = () => (
+    <div className={`burger-menu ${isMenuVisible ? 'visible' : ''}`}>
+      <div className="burger-menu__overlay" />
+      <div className="burger-menu__container">
+        <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/"
+          onClick={() => setIsMenuVisible(false)}
+        >
+          Главная
+        </NavLink>
+        <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/movies"
+          onClick={() => setIsMenuVisible(false)}
+        >
+          Фильмы
+        </NavLink>
+        <NavLink
+          className="burger__link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/saved-movies"
+          onClick={() => setIsMenuVisible(false)}
+        >
+          Сохраненные фильмы
+        </NavLink>
+        <NavLink
+          className="burger__link burger__link_profile"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/profile"
+          onClick={() => setIsMenuVisible(false)}
+        >
+          Аккаунт
+          <span className="header__profile_decor" />
+        </NavLink>
+      </div>
+    </div>
+  );
 
   const burgerButton = () => (
     <>
