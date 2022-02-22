e9b20a3c4a50ae531af1124e77be056d7151f3e9
Andrey <larikov174@gmail.com>
2022-02-22 20:12:45 +0500

    feat: add preloader

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,16 +1,40 @@
 import './Movies.css';
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 
 function Movies() {
-  return (
-    <section className="movies">
-      <SearchForm />
-      <MoviesCardList />
-    </section>
-  );
+  const length = 18;
+  const moviesDB = Array(length).fill({
+    image: '../../images/default-picture.png',
+    description: 'Название фильма',
+    duration: '1ч 17м',
+  });
+  const [initData, setInitData] = useState(null);
+
+  setTimeout(() => {
+    setInitData(moviesDB);
+  }, 3000);
+
+  const renderData = () => {
+    if (initData) {
+      return (
+        <>
+          <SearchForm />
+          <MoviesCardList />
+        </>
+      );
+    }
+    return (
+      <>
+        <SearchForm />
+        <Preloader />
+      </>
+    );
+  };
+
+  return <section className="movies">{renderData()}</section>;
 }
 
 export default Movies;
