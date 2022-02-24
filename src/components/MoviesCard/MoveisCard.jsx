006be94e6f50f64eca45c342ed9517c236524b45
Andrey Larikov <larikov174@gmail.com>
2022-02-24 11:44:25 +0500

    feat: add saved movie mark

--- src/components/MoviesCard/MoveisCard.jsx
+++ src/components/MoviesCard/MoveisCard.jsx
@@ -1,18 +1,35 @@
 import './MoviesCard.css';
 import './SFX.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function MoviesCard({ duration, description, image, onCardClick }) {
+  const [isSaved, setIsSaved] = useState(false);
   // TODO: добавить провеку результатов сохранения фильма в базу, пока всегда успешно
-  const handleCardClick = () => onCardClick({ type: 'success', title: 'Сохранение успешно!', visible: true });
+  const handleCardClick = () => {
+    setIsSaved(true);
+    onCardClick({ type: 'success', title: 'Сохранение успешно!', visible: true });
+  };
+
+  const renderCardStyle = () => {
+    if (isSaved) {
+      return (
+        <div className="movies-card__overlay movies-card__overlay_saved" role="presentation">
+          <span className="movies-card__decor movies-card__decor_saved" />
+        </div>
+      );
+    }
+    return (
+      <div className="movies-card__overlay" onClick={handleCardClick} role="presentation">
+        <span className="movies-card__decor" />
+      </div>
+    );
+  };
 
   return (
     <article className="movies-card">
       <div className="movies-card__poster">
         <img className="movies-card__image" alt="Обложка фильма" src={image} />
-        <div className="movies-card__overlay" onClick={handleCardClick} role="presentation">
-          <span className="movies-card__decor" />
-        </div>
+        {renderCardStyle()}
       </div>
       <div className="movies-card__info">
         <h2 className="movies-card__title">{description}</h2>
