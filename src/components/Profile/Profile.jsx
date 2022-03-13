3deca171c7853e7ed92f589cf3b09511deb28812
Andrey Larikov <larikov174@gmail.com>
2022-03-13 23:25:10 +0500

    feat: add profile form validation

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,63 +1,73 @@
 import './Profile.css';
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import CurrentUserContext from '../../contexts/CurrentUserContext';
+import useFormWithValidation from '../../utils/useFormWithValidation';
 import { CAPTION } from '../../utils/const';
 
 function Profile({ onSignOut, onUpdate }) {
   const user = useContext(CurrentUserContext);
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const onNameChange = (e) => setName(e.target.value);
-  const onEmailChange = (e) => setEmail(e.target.value);
+  const { values, handleChange, errors, setValues, isValid, resetForm } = useFormWithValidation();
+
+  const handleChangeInput = (e) => handleChange(e);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    return user && onUpdate({ name, email });
+    onUpdate({ email: values.email, name: values.name });
+    resetForm();
+  };
+
+  const handleSignOut = () => {
+    onSignOut();
+    resetForm();
   };
 
   useEffect(() => {
-    if (user){
-      setName(user.name);
-      setEmail(user.email);
-    }
-  }, [user]);
+    setValues(user);
+  }, [user, setValues]);
 
   return (
     <section className="profile">
-      <h1 className="profile__title">Привет, {name}!</h1>
-      <form className="profile__content" onSubmit={handleSubmit}>
+      <h1 className="profile__title">Привет, {user.name}!</h1>
+      <form className="profile__content" onSubmit={handleSubmit} noValidate>
         <div className="profile__row">
           <label htmlFor="userName" className="profile__text profile__text_subtitle">
             {CAPTION.NAME}
           </label>
           <input
-            onChange={onNameChange}
             type="text"
+            name="name"
+            id="userName"
             className="profile__text profile__input"
             placeholder="Введите имя..."
-            id="userName"
-            value={name || 'Введите имя...'}
+            value={values.name || ''}
+            pattern="^[A-Za-zА-Яа-яЁё\s-]{1,}$"
+            minLength="2"
+            onChange={handleChangeInput}
             required
           />
         </div>
+        <span className="profile__text profile__error">{errors.name}</span>
         <div className="profile__row">
           <label htmlFor="userEmail" className="profile__text profile__text_subtitle">
             {CAPTION.EMAIL}
           </label>
           <input
             type="email"
-            name="userEmail"
+            name="email"
             id="userEmail"
             className="profile__text profile__input"
             placeholder="Введите email..."
-            value={email || 'Введите email...'}
-            onChange={onEmailChange}
+            value={values.email || ''}
+            pattern="^[a-z0-9+_.-]+@[a-z0-9.-]+\.[a-z]+$"
+            onChange={handleChangeInput}
             required
           />
         </div>
-        <button type="submit" className="profile__button">
+        <span className="profile__text profile__error">{errors.email}</span>
+        <button type="submit" className="profile__button" disabled={!isValid}>
           {CAPTION.EDIT}
         </button>
-        <button className="profile__link" type="button" onClick={onSignOut}>
+        <button className="profile__link" type="button" onClick={handleSignOut}>
           {CAPTION.SIGN_OUT}
         </button>
       </form>
